b82937292083db3457226be5a0a9d7c4da89fd55
Buddhadeb Chhetri <buddhadebchhetri6@gmail.com>
2021-12-29 16:53:36 +0530

    Edited some text🤘🤘

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ fetch('https://icanhazdadjoke.com/slack')
   .then((data) => data.json())
   .then((jokeData) => {
       const joketext= jokeData.attachments[0].text;
-      const jokeElement= document.getElementById("jokeElemen");
+      const jokeElement= document.getElementById("jokeElement");
       jokeElement.innerHTML=joketext;
   })
 
